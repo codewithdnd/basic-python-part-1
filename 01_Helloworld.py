862cb446a1968a097f80b2631808aca274994585
codewithdnd <100120285+codewithdnd@users.noreply.github.com>
2022-02-27 11:32:20 +0530

    Add files via upload

---
+++ 01_Helloworld.py
@@ -0,0 +1,5 @@
+# TO write Hello world
+print("Hello World")
+
+# to write a sentance 10 times
+print("CodewithDND\n"*10)
---
+++ 02_Addtion.py
@@ -0,0 +1,4 @@
+a = input('Enter a Value of A: ')
+b = input('Enter a Value of B: ')
+c = float(a) + float (b)
+print("The Addition is : ",c)
